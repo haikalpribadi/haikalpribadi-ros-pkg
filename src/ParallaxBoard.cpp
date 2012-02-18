f3b5693930a00609ff7f255916b14e79cdd27f66
Haikal Pribadi <haikal.pribadi@gmail.com>
2012-02-19 05:28:44 +0800

    Initial compile

---
+++ src/ParallaxBoard.cpp
@@ -0,0 +1,21 @@
+/* 
+ * File:   ParallaxBoard.cpp
+ * Author: haikalpribadi
+ * 
+ * Created on February 19, 2012, 5:04 AM
+ */
+#include "ros/ros.h"
+#include "ParallaxBoard.h"
+
+ParallaxBoard::ParallaxBoard() {
+}
+
+ParallaxBoard::ParallaxBoard(const ParallaxBoard& orig) {
+}
+
+ParallaxBoard::~ParallaxBoard() {
+}
+
+int main(int argc, char** argv){
+
+}
